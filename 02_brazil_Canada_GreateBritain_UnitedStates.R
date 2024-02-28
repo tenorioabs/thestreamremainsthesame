752977f5e0f61b6cfeb1682706748b013051ea75
Jose Tenorio Abs Junior <tenorioabs@hotmail.com>
2024-02-28 12:24:51 -0300

    Nova URL

--- 02_brazil_Canada_GreateBritain_UnitedStates.R
+++ 02_brazil_Canada_GreateBritain_UnitedStates.R
@@ -25,7 +25,8 @@ url_m3u8 <- list("https://i.mjh.nz/Plex/ca.m3u8"="Canada",
                  "https://iptv-org.github.io/iptv/countries/us.m3u"="United States",
                  "https://iptv-org.github.io/iptv/countries/uk.m3u"="Great Britain",
                  "https://lib.bz"="Brazil",
-                 "http://m3u4u.com/m3u/p192y734qvuvvvgwymeg"="Brazil")
+                 "http://m3u4u.com/m3u/p192y734qvuvvvgwymeg"="Brazil",
+                 "http://m3u4u.com/m3u/69wkng49r8svvxg3yq8g"="Brazil")
 
 processa_url <- function(url, nome_grupo) {
   response <- tryCatch({
