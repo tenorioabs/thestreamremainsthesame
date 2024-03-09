2230002492b7336ba3e8ae3c04624d707de09ef3
Jose Tenorio Abs Junior <41520732+tenorioabs@users.noreply.github.com>
2024-03-08 21:41:17 -0300

    Update 01_plex_pluto_samsung_com_novo_grupo.R

--- 01_plex_pluto_samsung_com_novo_grupo.R
+++ 01_plex_pluto_samsung_com_novo_grupo.R
@@ -203,7 +203,13 @@ novos_canais <- c("#EXTINF:-1 tvg-id=\"MTV.jp\" tvg-logo=\"https://upload.wikime
                   "#EXTINF:-1,tvg-id=\"OurVinylTV\" tvg-logo=\"https://pbs.twimg.com/profile_images/1108077246999392258/e1rqU54I_400x400.png\" group-title=\"Music\",OurVinylTV Mexico\nhttps://ourvinyltv-ourvinyltv-1-mx.tcl.wurl.tv/playlist.m3u8",
                   "#EXTINF:-1 tvg-id=\"BandNews.br\" tvg-logo=\"https://i.imgur.com/WwSAtkh.png\" group-title=\"Brazil\",Band News\nhttps://cdn2.connectbr.com.br/Band-News/tracks-v1a1/playlist.m3u8",
                   "#EXTINF:-1 tvg-id=\"CNNBrasil.br\" tvg-logo=\"https://i.imgur.com/5AK7gLc.png\" group-title=\"Brazil\",CNN Brasil\nhttp://video01.soultv.com.br/cnnbrasil/cnnbrasil/playlist.m3u8",
-                  "#EXTINF:-1 tvg-id=\"GloboNews.br\" tvg-logo=\"https://upload.wikimedia.org/wikipedia/commons/8/89/Logotipo_da_GloboNews.png\" group-title=\"Brazil\",Globo News\nhttp://209.222.97.92:16436/GloboNews")
+                  "#EXTINF:-1 tvg-id=\"GloboNews.br\" tvg-logo=\"https://upload.wikimedia.org/wikipedia/commons/8/89/Logotipo_da_GloboNews.png\" group-title=\"Brazil\",Globo News\nhttp://209.222.97.92:16436/GloboNews",
+                  "#EXTINF:-1 tvg-id=\"4music.uk\" tvg-name=\"UK - 4Music\" tvg-logo=\"http://attp.ddns.net:25461/images/3716d07c2ea18053da67e42f23510673.png\" group-title=\"Music\",4Music\nhttp://2hubs.ddns.net:25461/Faucon1tvMT/g8pHKUYxwDhx/59948",
+                  "#EXTINF:-1 tvg-id=\"Cnn.us\" tvg-name=\"USA - CNN\" tvg-logo=\"http://attp.ddns.net:25461/images/3b28681ca92c1a20a634cecda3944f7d.png\" group-title=\"USA\",CNN USA\nhttp://2hubs.ddns.net:25461/Faucon1tvMT/g8pHKUYxwDhx/57214",
+                  "#EXTINF:-1 tvg-id=\"Cnn.ca\" tvg-name=\"CA - CNN HD\" tvg-logo=\"http://attp.ddns.net:25461/images/92de8241eb80455f9f1eb8bf008a79eb.png\" group-title=\"Canada\",CNN CA HD\nhttp://2hubs.ddns.net:25461/Faucon1tvMT/g8pHKUYxwDhx/61130",
+                  "#EXTINF:-1 tvg-id=\"Cnn.ca\" tvg-name=\"CA - CNN INTERNATIONAL  HD\" tvg-logo=\"http://attp.ddns.net:25461/images/92de8241eb80455f9f1eb8bf008a79eb.png\" group-title=\"Canada\",CNN INTERNATIONAL CA HD\nhttp://2hubs.ddns.net:25461/Faucon1tvMT/g8pHKUYxwDhx/61131",
+                  "#EXTINF:-1 tvg-id=\"Cnn.us\" tvg-name=\"USA - CNN HD\" tvg-logo=\"http://attp.ddns.net:25461/images/8b0b0df3207d9e82299feba06c904a17.png\" group-title=\"USA\",CNN USA HD\nhttp://2hubs.ddns.net:25461/Faucon1tvMT/g8pHKUYxwDhx/58495")
+
 conteudo_concatenado <- c(conteudo_concatenado, novos_canais)
 
 # Escrever o conteúdo concatenado em um novo arquivo
