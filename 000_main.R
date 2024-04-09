92ec56954e775cbb3bf39c130311a664698db5bd
tenorioabs <tenorioabs@hotmail.com>
2024-04-09 08:20:09 -0300

    atualizacao_20240409082008

---
+++ 000_main.R
@@ -0,0 +1,43 @@
+source("099_instala_carrega_pacotes.R")
+source("098_funcoes.R")
+options(warn = -1)
+# Solicitar ao usuário que insira um valor
+# opcoes <- as.numeric(readline("Escolha 1 = Reduced ou 2 = Full:"))
+opcoes <- c(1, 2)
+
+for (i in 1:length(opcoes)) {
+  valor_numerico <- i
+  source("001_download_concatenacao_urls.R")
+  tabula_group_title(nome_coluna)
+  source("002_cria_music_salva_arquivo.R")
+  source("003_insere_canais_manualmente_concatena_resultados_buscados.R")
+  source("004_cria_index_epg_no_m3u8.R")
+  source("005_cria_grupos.R")
+  #source("006_double_check_canais.R")
+  source("007_atribui_logo_remove_repetidos.R")
+  tabula_group_title(nome_coluna)
+
+  if (valor_numerico == 2) {
+    source("008_testa_links_m3u8_group.R")
+    tabula_group_title(nome_coluna)
+  }
+  
+  if (valor_numerico==1) {
+    retry(source("009_cria_xml.R"), max = Inf)
+  }
+  
+  try(file.remove("canais_encontrados_modificados.m3u8"), silent = T)
+  
+  info_so <- Sys.info()
+  
+  dia_hora <- Sys.time()
+  dia_hora <- str_replace_all(string = dia_hora, pattern = "-", replacement = "")
+  dia_hora <- str_replace_all(string = dia_hora, pattern = ":", replacement = "")
+  dia_hora <- str_replace_all(string = dia_hora, pattern = " ", replacement = "")
+  
+  if (info_so['sysname'] == 'Windows') {
+    github_windows(paste0("atualizacao_", dia_hora))
+  } else if (info_so['sysname'] == 'Linux') {
+    github_linux(paste0("atualizacao_", dia_hora))
+  }
+}
---
+++ 099_instala_carrega_pacotes.R
@@ -0,0 +1,27 @@
+# Obtendo informações do sistema
+info_so <- Sys.info()
+
+  pacotes <- c("dplyr",
+               "httr",
+               "R.utils",
+               "stringr",
+               "writexl",
+               "xml2",
+               "readxl",
+               "future",
+               "future.apply",
+               "progressr",
+               "xml2",
+               "R.utils")
+
+# Verificando se o sistema é Windows ou Linux
+if(sum(as.numeric(!pacotes %in% installed.packages())) != 0){
+    instalador <- pacotes[!pacotes %in% installed.packages()]
+    for(i in 1:length(instalador)) {
+      install.packages(instalador, dependencies = T)
+      break()}
+    sapply(pacotes, require, character = T) 
+  } else {
+    sapply(pacotes, require, character = T) 
+  }
+
