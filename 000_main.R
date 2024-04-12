a3e3fc632498b0727ed2f1bc45feed4b7592ce8a
tenorioabs <tenorioabs@hotmail.com>
2024-04-12 14:05:48 -0300

    atualizacao_20240412140547

--- 000_main.R
+++ 000_main.R
@@ -3,7 +3,7 @@ source("098_funcoes.R")
 options(warn = -1)
 # Solicitar ao usuário que insira um valor
 # opcoes <- as.numeric(readline("Escolha 1 = Reduced ou 2 = Full:"))
-opcoes <- c(1)
+opcoes <- c(1, 2)
 
 for (i in 1:length(opcoes)) {
   valor_numerico <- i
@@ -15,17 +15,17 @@ for (i in 1:length(opcoes)) {
   source("005_cria_grupos.R")
   #source("006_double_check_canais.R")
   source("007_atribui_logo_remove_repetidos.R")
-  source("010_remove_grupo_omitir.R")
   
   tabula_group_title(nome_coluna)
 
-  # if (valor_numerico == 2) {
-  #   source("008_testa_links_m3u8_group.R")
-  #   tabula_group_title(nome_coluna)
-  # }
+  if (valor_numerico == 2) {
+    source("008_testa_links_m3u8_group.R")
+    tabula_group_title(nome_coluna)
+  }
   
   if (valor_numerico==1) {
     retry(source("009_cria_xml.R"), max = Inf)
+    try(source("010_remove_grupo_omitir.R"), silent = T)
   }
   
   try(file.remove("canais_encontrados_modificados.m3u8"), silent = T)
