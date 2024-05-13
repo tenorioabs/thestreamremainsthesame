1524ba4de9a0836986f86d3a35a880aa697e082f
Pira <tenorioabs>
2024-05-13 10:06:25 -0300

    mint

---
+++ iptv.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+git clone https://github.com/tenorioabs/thestreamremainsthesame.git && cd thestreamremainsthesame && Rscript 000_main.R && cd .. && sudo rm -r /home/pira/thestreamremainsthesame/ && exit
