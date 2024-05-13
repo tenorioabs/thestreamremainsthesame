96fc5037d06d34e3aa32312f6b25e18fa7eefe85
Pira <tenorioabs>
2024-05-13 10:22:48 -0300

    atualizacao_20240513102248

--- iptv.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-git clone https://github.com/tenorioabs/thestreamremainsthesame.git && cd thestreamremainsthesame && Rscript 000_main.R && cd .. && sudo rm -r /home/pira/thestreamremainsthesame/ && exit
